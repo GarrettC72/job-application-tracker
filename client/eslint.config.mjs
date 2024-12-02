3c6f46a7e116146d8f4805a48dacc5f6b460a57f
Garrett Chen <garrettchen2@gmail.com>
2024-12-02 18:12:18 -0500

    Updated config file for eslint in client

---
+++ client/eslint.config.mjs
@@ -0,0 +1,70 @@
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactHooks from "eslint-plugin-react-hooks";
+import react from "eslint-plugin-react";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    // config with just ignores is the replacement for `.eslintignore`
+    ignores: [
+      "**/dist",
+      "**/.eslintrc.cjs",
+      "**/node_modules",
+      "**/__generated__",
+    ],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      "@stylistic": stylistic,
+      "react-refresh": reactRefresh,
+      "react-hooks": reactHooks,
+      react: react,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
+      "@stylistic/semi": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "@typescript-eslint/restrict-plus-operands": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "no-case-declarations": "off",
+      "@typescript-eslint/array-type": ["error", { default: "array-simple" }],
+      "react/react-in-jsx-scope": 0,
+      quotes: ["error", "double"],
+      eqeqeq: "error",
+    },
+  }
+);
